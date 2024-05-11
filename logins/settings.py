caf5bdcd051e41d1850c10a839b00eda04122e72
shashankkaran <shashankkaran999@gmail.com>
2024-05-12 05:06:51 +0530

    done

--- logins/settings.py
+++ logins/settings.py
@@ -31,7 +31,7 @@ EMAIL_PORT = EMAIL_PORT
 SECRET_KEY = 'django-insecure-mvje+gksl_ve%0d7(u8cu5p3x$ym%@y23+ujwk__o6y+nk^q$i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
