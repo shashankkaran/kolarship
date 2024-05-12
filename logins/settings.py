82055faa40c4b066c7906be3a568d60c7b317862
shashankkaran <shashankkaran999@gmail.com>
2024-05-12 06:35:26 +0530

    done

--- logins/settings.py
+++ logins/settings.py
@@ -14,6 +14,11 @@ import os
 from pathlib import Path
 import dj_database_url
 from . info import *
+import mimetypes
+
+
+# A Bug is was encountering
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
