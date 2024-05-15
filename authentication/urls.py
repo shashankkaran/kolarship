dcc067e58f03a85740b11a14aab59d8ba9dab1af
shashankkaran <shashankkaran999@gmail.com>
2024-05-15 16:07:12 +0530

    debuged

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -37,8 +38,10 @@ urlpatterns = [
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-]
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+   
+] 
 
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
     
--- logins/settings.py
+++ logins/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'authentication/static'),
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
 LOGIN_URL='/login/'
 
